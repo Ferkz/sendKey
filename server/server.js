6274f38257934387e6565a363af646d1c98038f6
Ferkz <fermon1952@gmail.com>
2022-08-29 11:50:49 -0400

    update-08

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
+
 const express = require ('express');
 const app = express();
+const path = require('path')
+const port = 3000
  
+//public
+//app.use(express.static.apply(path.join(__dirname,"public")))
+app.set('views', path.join(__dirname,'../client/src/','views'))
+app.set('public',path.join(__dirname,'../public','public'))
+app.set('view engine','ejs')
 app.use('/',require('../client/src/routers/routers'));
 
-app.listen(3000);
-console.log('servidor on')
+
+app.listen(port,()=>{
+    console.log("servidor rodando na porta",port);
+})
