1c4d7361cae443c898c69468b75b9b3193dfe79c
Jefferson <nhac.developer@gmail.com>
2022-09-20 22:24:45 -0300

    Fix erro socket.io

--- config/webSocket.io.js
+++ config/webSocket.io.js
@@ -1,5 +1,27 @@
-const io = require("../server");
+const ws = require("socket.io");
 
-io.on("connection",(socket) =>{
-    console.log('teste',socket.id);
-})
+module.exports = (server)=>{
+    const io = ws(server)
+
+    // Escuta o evento de conexão   
+    io.on("connect", (socket)=>{
+        console.log(`Socket conectado ${socket.id}`);
+
+        /**
+         * Compreendendo eventos
+         * Você irá dá nome a eles e manipular
+         * Existe o emissor e o ouvinte
+         * ws.on é ouvinte, ele ouve e executa uma função
+         * ws.on("teste", (args)=>{ console.log(args) })
+         * ws.emit é um emissor de evento 
+         * ws.emit("teste", 10)
+         * 
+         * Ambas as funções funciona no frontend como no backend.
+         */
+
+    })
+
+
+    // Retorna o io para poder executar ele fora da função
+    return io
+}
